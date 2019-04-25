17d095dfafe373b3d6c038dd2ae3e7c533ba8760
Christon Cardoza <Christoncardoza96@gmail.com>
2019-04-25 23:26:18 +0530

    updated SMTP

--- Basic/smtp.py
+++ Basic/smtp.py
@@ -1,25 +1,24 @@
 import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.mime.application import MIMEApplication
 from getpass import getpass
 server = smtplib.SMTP("smtp.gmail.com",587)
 server.ehlo()
 server.starttls()
-username= "christoncardoza33@gmail.com.com"
+username = 'christoncardoza33@gmail.com'
 password = getpass()
 server.login(username,password)
-#msg = "Hi"
-# server.sendmail(username,username, msg)
+#msg = "Hi Man, Welcome to SMTP"
+#server.sendmail(username,username,msg)
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
 msg = MIMEMultipart()
 msg['From'] = username
-msg['To'] = 'christoncardoza96@gmail.com'
-msg['Subject'] = 'logo'
-text = 'check out the logo!'
+msg['To'] = username
+msg['Subject'] = "Testing SMTP Connection"
+text = 'Connection Successful. PFA'
 msg.attach(MIMEText(text))
-with open('logo.png','rb')as f: 
+with open(r"C:\Users\Christon\Desktop\Untitled.png",'rb') as f:
     part = MIMEApplication(f.read())
-    part.add_header('Content-Disposition','attachment',filename="logo.png")
+    part.add_header('Content-Disposition', 'attachment', filename='mypic.png')
     msg.attach(part)
 server.sendmail(msg['From'],msg['To'],msg.as_string())
-     
